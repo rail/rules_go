2a778e92663acd6ce98f73d741506234ddea136c
Mari Staib <mari@cockroachlabs.com>
2022-04-04 17:33:19 -0400

    Translate paths in compilepkg output.
    When cgo2 is involved in a build, the go srcs are linked/copied
    into a temporary directory to keep everything in the same
    directory. Those paths are unrecognizable to IDEs, being absolute
    paths outside the project directory.
    
    This adds a translation step to replace paths in the compiler output,
    translating them back to the input paths that were given to the tool
    in the first place.
    
    This doesn't affect -trimpath, which affects runtime error messages
    but not compilation-time error messages; for the same reason, this
    change can't use that flag to do its dirty work.
    
    Fixes cockroachdb/cockroach#76377.
    
    Release note: None

--- go/tools/builders/compilepkg.go
+++ go/tools/builders/compilepkg.go
@@ -267,14 +267,19 @@ func compileArchive(
 	// If we have cgo, generate separate C and go files, and compile the
 	// C files.
 	var objFiles []string
+	var goSrcsMapping []pathPair
 	if cgoEnabled && haveCgo {
 		// TODO(#2006): Compile .s and .S files with cgo2, not the Go assembler.
 		// If cgo is not enabled or we don't have other cgo sources, don't
 		// compile .S files.
 		var srcDir string
-		srcDir, goSrcs, objFiles, err = cgo2(goenv, goSrcs, cgoSrcs, cSrcs, cxxSrcs, objcSrcs, objcxxSrcs, nil, hSrcs, packagePath, packageName, cc, cppFlags, cFlags, cxxFlags, objcFlags, objcxxFlags, ldFlags, cgoExportHPath)
+		srcDir, goSrcsMapping, objFiles, err = cgo2(goenv, goSrcs, cgoSrcs, cSrcs, cxxSrcs, objcSrcs, objcxxSrcs, nil, hSrcs, packagePath, packageName, cc, cppFlags, cFlags, cxxFlags, objcFlags, objcxxFlags, ldFlags, cgoExportHPath)
 		if err != nil {
 			return err
+		}
+		goSrcs = make([]string, len(goSrcsMapping))
+		for i, v := range goSrcsMapping {
+			goSrcs[i] = v.workingPath
 		}
 
 		gcFlags = append(gcFlags, fmt.Sprintf("-trimpath=%s=>%s", abs(srcDir), packagePath))
@@ -395,7 +400,7 @@ func compileArchive(
 	}
 
 	// Compile the filtered .go files.
-	if err := compileGo(goenv, goSrcs, packagePath, importcfgPath, embedcfgPath, asmHdrPath, symabisPath, gcFlags, outPath); err != nil {
+	if err := compileGo(goenv, goSrcs, packagePath, importcfgPath, embedcfgPath, asmHdrPath, symabisPath, gcFlags, goSrcsMapping, outPath); err != nil {
 		return err
 	}
 
@@ -465,7 +470,7 @@ func compileArchive(
 	return appendFiles(goenv, outXPath, []string{pkgDefPath})
 }
 
-func compileGo(goenv *env, srcs []string, packagePath, importcfgPath, embedcfgPath, asmHdrPath, symabisPath string, gcFlags []string, outPath string) error {
+func compileGo(goenv *env, srcs []string, packagePath, importcfgPath, embedcfgPath, asmHdrPath, symabisPath string, gcFlags []string, paths []pathPair, outPath string) error {
 	args := goenv.goTool("compile")
 	args = append(args, "-p", packagePath, "-importcfg", importcfgPath, "-pack")
 	if embedcfgPath != "" {
@@ -482,7 +487,7 @@ func compileGo(goenv *env, srcs []string, packagePath, importcfgPath, embedcfgPa
 	args = append(args, "--")
 	args = append(args, srcs...)
 	absArgs(args, []string{"-I", "-o", "-importcfg"})
-	return goenv.runCommand(args)
+	return goenv.runCommandAndReplacePaths(args, paths)
 }
 
 func runNogo(ctx context.Context, workDir string, nogoPath string, srcs []string, deps []archive, packagePath, importcfgPath, outFactsPath string) error {
